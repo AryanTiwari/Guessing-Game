10797d1d5249dd99c8d6804afe6cb6f7af8b38d8
AryanTiwari <powerdrgn@gmail.com>
2018-06-06 09:39:39 -0400

    Now it shows your previously guessed numbers

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
@@ -9,36 +9,41 @@ public class RandomNumber {
 	public static void main(String[] args) {
 
 		// Make random number
-		Random number = new Random();
-		int number1 = number.nextInt(101);
-		System.out.println(number1);
-		
+		int number = (int) (Math.random() * 100);
+		ArrayList<Integer> numbersGuessed = new ArrayList<Integer>();
+
 		// Guess the number
 		System.out.println("Guess a number from 0 - 100");
 
 		// Guess loop
-		for (int guesses = 0; guesses < 10; guesses++) {
+		for (int guesses = 10; guesses > 0; guesses--) {
 
 			// Guess stuff
 			int guess = input.nextInt();
+			numbersGuessed.add(guess);
 
-			if (guess == number1 && guesses == 0) {
-				JOptionPane.showMessageDialog(null, "Wow you got it first try!");
+			if (guesses == 1) {
+				System.out.println("You ran out of guesses");
+				System.out.println("The number was " + number);
+				return;
+			}
+			if (guess == number && guesses == 0) {
+				JOptionPane.showMessageDialog(null, "Wow you got it first try!", "Correct", JOptionPane.PLAIN_MESSAGE);
 			} else if (guess < 0 || guess > 100) {
 				System.out.println("Your guess should be from 0 - 100\n");
 				guesses = guesses - 1;
-			} else if (guess == number1) {
+			} else if (guess == number) {
 				JOptionPane.showMessageDialog(null, "Good job! You guessed the number!");
-				break;
-			} else if (guess < number1) {
+			} else if (guess < number) {
 				System.out.println("Too low, try again");
-				System.out.println("You have " + (9 - guesses) + " guesses left\n");
-			} else if (guess > number1) {
+				System.out.println("You have " + (guesses - 1) + " guesses left");
+				System.out.println("You have previously guessed: " + numbersGuessed + "\n");
+			} else if (guess > number) {
 				System.out.println("Too high, try again");
-				System.out.println("You have " + (9 - guesses) + " guesses left\n");
-			} else if (guess == 0) {
-				System.out.println("You couldn't guess the number");
+				System.out.println("You have " + (guesses - 1) + " guesses left");
+				System.out.println("You have previously guessed: " + numbersGuessed + "\n");
 			}
 		}
+
 	}
 }
